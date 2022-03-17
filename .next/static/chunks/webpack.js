52651796b7a77306adfd2c86cdac9ecc98ac2cfb
anshul shah <anshul.shah@brainvire.com>
2022-03-17 11:44:37 +0530

    commit 2

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "05b2ba5ea0308b55d960";
+/******/ 	var hotCurrentHash = "7ab8d8944223d708d6b9";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
